1fedbbccae81bba8949af5e432b538a78f4a9e89
se0yu <pr924133@gmail.com>
2025-04-04 11:32:29 +0900

    docs: README.md, Erd, schedule.sql 생성

---
+++ schedule.sql
@@ -0,0 +1,22 @@
+CREATE TABLE schedule(
+                         id BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '일정 식별자',
+                         title VARCHAR(255) COMMENT '일정 제목',
+                         contents TEXT COMMENT '일정 내용',
+                         created_at DATETIME(6) COMMENT '작성일',
+                         updated_at DATETIME(6) COMMENT '수정일'
+);
+
+CREATE TABLE user(
+                     id BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '유저 식별자',
+                     username VARCHAR(255) COMMENT '이름',
+                     email VARCHAR(255) COMMENT '이메일',
+                     password VARCHAR(255) COMMENT '비밀번호',
+                     created_at DATETIME(6) COMMENT '작성일',
+                     updated_at DATETIME(6) COMMENT '수정일'
+);
+
+
+CREATE TABLE comments(
+                         id BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '댓글 식별자',
+                         content TEXT COMMENT '댓글 내용'
+);
