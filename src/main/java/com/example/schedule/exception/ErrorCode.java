70e37f86ff16ed93eaf5485bb77c106425718cc8
se0yu <pr924133@gmail.com>
2025-07-24 18:37:38 +0900

    feat: comment 커스텀 에러 코드 추가

--- src/main/java/com/example/schedule/exception/ErrorCode.java
+++ src/main/java/com/example/schedule/exception/ErrorCode.java
@@ -25,7 +25,9 @@ public enum ErrorCode {
 
 
     //comment
-    COMMENT_NOT_FOUND(404,"Not Found","CM001","댓글을 찾을 수 없습니다");
+    COMMENT_NOT_FOUND(404,"Not Found","C001","댓글을 찾을 수 없습니다"),
+    UNAUTHORIZED_COMMENT_UPDATE(403,"Forbidden","C002","댓글 작성자만 댓글을 수정할 수 있습니다."),
+    UNAUTHORIZED_COMMENT_DELETE(403,"Forbidden","C003","댓글 작성자 혹은 게시글 작성자만 댓글을 삭제할 수 있습니다.");
 
     private final int status;
     private final String error;
